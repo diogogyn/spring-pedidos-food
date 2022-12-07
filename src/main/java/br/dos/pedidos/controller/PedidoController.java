aa544b3834f753a339f88d872c7a9134ce675516
Diogo <diogogyn100@gmail.com>
2022-12-07 12:14:47 -0300

    incluindo gateway

--- src/main/java/br/dos/pedidos/controller/PedidoController.java
+++ src/main/java/br/dos/pedidos/controller/PedidoController.java
@@ -4,6 +4,7 @@ import br.dos.pedidos.dto.PedidoDto;
 import br.dos.pedidos.dto.StatusDto;
 import br.dos.pedidos.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -56,4 +57,10 @@ public class PedidoController {
         return ResponseEntity.ok().build();
 
     }
+
+    @GetMapping("/porta")
+    public String retornaPorta(@Value("${local.server.port}") String porta){
+        return String.format("Requisição respondida pela instância executando na porta %s", porta);
+    }
+
 }
